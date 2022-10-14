bdb024076ae1eee8bc1f960724773099571eda5c
chan0310 <57311977+chan0310@users.noreply.github.com>
2022-10-14 11:49:24 +0900

    Create python.py

---
+++ src/python.py
@@ -0,0 +1,11 @@
+import math
+abc=int(input())
+def adfadf(M):
+    for t in range(M):
+        i=list(str(t))
+        m=list(map(int,i))
+        if t+sum(m)==M:
+            return(t)
+    return 0
+
+print(adfadf(abc))
